c718004480d5e97b9f1e11cf9bf04dc7daeddea0
Muhammad  Junaid <junaid123.jk78@gmail.com>
2020-06-26 15:28:13 +0500

    Initial Commit

---
+++ 16-Day-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  16-Day-SwiftUI
+//
+//  Created by Jhon Khan on 26/06/2020.
+//  Copyright © 2020 Jhon Khan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
